33f8d25dd9e85dfbfbc5038e557c382abd98dcd9
NoobSploit <87351604+KholsAdminHouse@users.noreply.github.com>
2022-07-17 19:37:09 -0600

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -17,31 +17,52 @@ local function esp(v)
             return loop:Disconnect()
         end
         
+		local name = function()
+            if v.Name == v.DisplayName then
+                return tostring(v.Name)
+            else
+                return ("%s [@%s]"):format(tostring(v.Name),tostring(v.DisplayName))
+            end
+        end
+		local pos = function()
+			if lp and lp.Character and lp.Character:FindFirstChildOfClass('Humanoid') and lp.Character:FindFirstChildOfClass('Humanoid').RootPart then
+				return math.round(tonumber((lp.Character:FindFirstChildOfClass('Humanoid').RootPart.CFrame.p - v.Character:FindFirstChildOfClass('Humanoid').RootPart.CFrame.p).Magnitude))
+			else
+				return 'N/A'
+			end
+		end
         local tpos,inview = CurrentCamera:WorldToViewportPoint(v.Character:FindFirstChildOfClass('Humanoid').RootPart.Position)
-        
-        text.Text = ("%s\n[%s] [%s/%s]"):format(
-		tostring(v),
-		math.round(tonumber((lp.Character:FindFirstChildOfClass('Humanoid').RootPart.CFrame.p - v.Character:FindFirstChildOfClass('Humanoid').RootPart.CFrame.p).Magnitude)),
-		math.round(v.Character:FindFirstChildOfClass('Humanoid').Health),
-		v.Character:FindFirstChildOfClass('Humanoid').MaxHealth)
 
+		local health = math.round(v.Character:FindFirstChildOfClass('Humanoid').Health)
+		local maxhealth = v.Character:FindFirstChildOfClass('Humanoid').MaxHealth
+        local teamcolor = BrickColor.new(tostring(v.TeamColor))
+        local color = Color3.new(teamcolor.r,teamcolor.g,teamcolor.b)
+        local teamcheck = function()
+            if lp.Team == v.Team then
+                return false
+            else
+                return inview
+            end
+        end
+        
+        text.Text = ("%s\n[%s] [%s/%s]"):format(name(),pos(),health,maxhealth)
         text.Size = 16
         text.Position = Vector2.new(tpos.X,tpos.Y)
 		text.Color = Color3.fromRGB(255, 255, 255)
 		text.Outline = true
 		text.Center = true
-		text.Visible = inview
+		text.Visible = teamcheck()
         text.ZIndex = 1
         
-        line.Visible = inview
+        line.Visible = teamcheck()
         line.From = Vector2.new(UserInputService:GetMouseLocation().X, UserInputService:GetMouseLocation().Y)
         line.To = Vector2.new(tpos.X,tpos.Y)
-        line.Color = Color3.fromRGB(255, 255, 255)
+        line.Color = color
         line.ZIndex = 1
     end)
 end
 
-for _,v in next, Players:GetPlayers() do
+for i,v in next, Players:GetPlayers() do
    	if v ~= lp then
        	esp(v)
 
@@ -52,10 +73,7 @@ for _,v in next, Players:GetPlayers() do
 end
 
 Players.PlayerAdded:Connect(function(plr)
-   	if plr ~= lp then
-       	esp(plr)
-       	plr.CharacterAdded:Connect(function()
-           	esp(plr)
-       	end)
-   	end
+    plr.CharacterAdded:Connect(function()
+        esp(plr)
+    end)
 end)
