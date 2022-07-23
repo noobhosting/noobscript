82d3651978af24672fb761fee39174e0039f92ab
NoobSploit <87351604+KholsAdminHouse@users.noreply.github.com>
2022-07-22 18:38:58 -0600

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -8,7 +8,22 @@ local CurrentCamera = workspace.CurrentCamera
 local function esp(v)
     local text = Drawing.new("Text")
     local line = Drawing.new("Line")
+    local friend = function()
+        if lp:IsFriendsWith(v.UserId) then
+            return true
+        end
+        return false
+    end
+    
     local loop
+
+    local name = function()
+        if v.Name == v.DisplayName then
+            return tostring(v.Name)
+        else
+            return ("%s [@%s]"):format(tostring(v.DisplayName),tostring(v.Name))
+        end
+    end
     
     loop = rs.Heartbeat:Connect(function()
         if not v then
@@ -21,14 +36,9 @@ local function esp(v)
             line.Visible = false
             return
         end
-        
-		local name = function()
-            if v.Name == v.DisplayName then
-                return tostring(v.Name)
-            else
-                return ("%s [@%s]"):format(tostring(v.DisplayName),tostring(v.Name))
-            end
-        end
+
+        local tpos,inview = CurrentCamera:WorldToViewportPoint(v.Character:FindFirstChildOfClass('Humanoid').RootPart.Position)
+
 		local pos = function()
 			if lp and lp.Character and lp.Character:FindFirstChildOfClass('Humanoid') and lp.Character:FindFirstChildOfClass('Humanoid').RootPart then
 				return math.round(tonumber((lp.Character:FindFirstChildOfClass('Humanoid').RootPart.CFrame.p - v.Character:FindFirstChildOfClass('Humanoid').RootPart.CFrame.p).Magnitude))
@@ -36,14 +46,21 @@ local function esp(v)
 				return 'N/A'
 			end
 		end
-        local tpos,inview = CurrentCamera:WorldToViewportPoint(v.Character:FindFirstChildOfClass('Humanoid').RootPart.Position)
 
 		local health = math.round(v.Character:FindFirstChildOfClass('Humanoid').Health)
 		local maxhealth = v.Character:FindFirstChildOfClass('Humanoid').MaxHealth
         local teamcolor = BrickColor.new(tostring(v.TeamColor))
-        local color = Color3.new(teamcolor.r,teamcolor.g,teamcolor.b)
+
+        local color = function()
+            if friend() then
+                return Color3.fromHSV(tick()%5/5,1,1)
+            else
+                return Color3.new(teamcolor.r,teamcolor.g,teamcolor.b)
+            end
+        end
+
         local teamcheck = function()
-            if lp.Team ~= nil and lp.Team == v.Team then
+        if lp.Team ~= nil and lp.Team == v.Team and not friend() then
                 return false
             else
                 return inview
@@ -53,7 +70,7 @@ local function esp(v)
         text.Text = ("%s\n[%s] [%s/%s]"):format(name(),pos(),health,maxhealth)
         text.Size = 16
         text.Position = Vector2.new(tpos.X,tpos.Y)
-		text.Color = color
+		text.Color = color()
 		text.Outline = true
 		text.Center = true
 		text.Visible = teamcheck()
@@ -62,7 +79,7 @@ local function esp(v)
         line.Visible = teamcheck()
         line.From = Vector2.new(UserInputService:GetMouseLocation().X, UserInputService:GetMouseLocation().Y)
         line.To = Vector2.new(tpos.X,tpos.Y)
-        line.Color = color
+        line.Color = color()
         line.ZIndex = 1
     end)
 end
